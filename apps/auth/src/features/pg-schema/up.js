8072e768b0857fab74a15d7e7957e97da729db2d
marcopeg <marco.pegoraro@gmail.com>
2022-02-17 07:25:18 +0100

    Move SQL logic into functions

--- apps/auth/src/features/pg-schema/up.js
+++ apps/auth/src/features/pg-schema/up.js
@@ -56,5 +56,96 @@ module.exports = (pg) =>
       PRIMARY KEY ("id")
     );
 
+    CREATE OR REPLACE FUNCTION 
+    get_session_claims(
+      PAR_uname TEXT,
+      PAR_tenant TEXT,
+      PAR_app TEXT
+    ) RETURNS TABLE (
+      "url" TEXT,
+      "tenants" JSON,
+      "apps" JSON
+    )
+    AS $$
+    BEGIN
+      RETURN QUERY
+      WITH
+      -- Centralize the Query parameters:
+      "params" AS (
+        SELECT
+          PAR_uname AS "user",
+          PAR_tenant AS "tenant",
+          PAR_app AS "app"
+      ),
+      -- Fetch data from multiple tables:
+      "app_data" AS (
+        SELECT "t1"."url"	AS "value"	
+        FROM "public"."apps" AS "t1"
+        INNER JOIN "public"."catalog" AS "t2" ON "t1"."name" = "t2"."name"
+        WHERE "t1"."name" = (SELECT "app" FROM "params")
+          AND "t2"."user" = (SELECT "user" FROM "params")
+          AND "t2"."tenant" = (SELECT "tenant" FROM "params")
+        LIMIT 1
+      ),
+      "tenants" AS (
+        SELECT "tenant" AS "value"
+        FROM "public"."tenants"
+        WHERE "user" = (SELECT "user" FROM "params")
+      ), 
+      "apps" AS (
+        SELECT "name" AS "value"
+        FROM "public"."catalog"
+        WHERE "user" = (SELECT "user" FROM "params")
+      )
+    
+      -- Combine multiple result into a single JSON response:
+      SELECT 
+        ( SELECT "value" FROM "app_data" ) AS "url",
+        (
+        SELECT array_to_json(array_agg("value")) AS "tenants"
+        FROM ( SELECT * FROM "tenants" ) t
+        ),
+        (
+        SELECT array_to_json(array_agg("value")) AS "apps"
+        FROM ( SELECT * FROM "apps" ) t
+        );
+    
+    END; $$
+    LANGUAGE plpgsql;
+
+    CREATE OR REPLACE FUNCTION 
+    build_session_token (
+      PAR_identityToken UUID,
+      PAR_claims JSON,
+      PAR_interval TEXT
+    ) RETURNS TABLE (
+      "delegate_token" UUID
+    )
+    AS $$
+    BEGIN
+      RETURN QUERY
+      WITH 
+        session_token AS (
+          INSERT INTO "public"."session_tokens"
+            ("identity_token", "claims") VALUES 
+            (PAR_identityToken, PAR_claims) 
+          ON CONFLICT ON CONSTRAINT "session_tokens_pkey"
+          DO UPDATE SET "created_at" = EXCLUDED."created_at"
+          RETURNING "id"
+        ),
+        refresh_token AS (
+          INSERT INTO "public"."refresh_tokens"
+            ("session_token", "expires_at") VALUES 
+            (
+              (SELECT "id" from "session_token"),
+              NOW() + PAR_interval::interval
+            ) 
+          RETURNING "id"
+        )
+        SELECT "id" AS "delegate_token" FROM refresh_token;
+    
+    END; $$
+    LANGUAGE plpgsql;
+
     COMMIT;
 `);
--- apps/auth/src/features/private-pages/fastify-handlers/open-page.js
+++ apps/auth/src/features/private-pages/fastify-handlers/open-page.js
@@ -1,83 +1,43 @@
-const GET_APP = `
-  SELECT "t1"."url" FROM "public"."apps" AS "t1"
-  LEFT JOIN "public"."catalog" AS "t2" ON "t1"."name" = "t2"."name"
-  WHERE "t1"."name" = $1 
-    AND "t2"."user" = $2
-    AND "t2"."tenant" = $3
-  LIMIT 1;
-`;
-
-const GET_REFRESH_TOKEN = `
-  WITH 
-    session_token AS (
-      INSERT INTO "public"."session_tokens"
-        ("identity_token", "claims") VALUES 
-        ($1, $2) 
-      ON CONFLICT ON CONSTRAINT "session_tokens_pkey"
-      DO UPDATE SET "created_at" = EXCLUDED."created_at"
-      RETURNING "id"
-    ),
-    refresh_token AS (
-      INSERT INTO "public"."refresh_tokens"
-        ("session_token", "expires_at") VALUES 
-        (
-          (SELECT "id" from "session_token"),
-          NOW() + $3::interval
-        ) 
-      RETURNING "id"
-    )
-    SELECT "id" AS "delegateToken" FROM refresh_token;
-`;
+const GET_SESSION_CLAIMS = `SELECT * FROM get_session_claims($1, $2, $3)`;
+const GET_REFRESH_TOKEN = `SELECT * FROM build_session_token($1, $2, $3)`;
 
 module.exports = async (request, reply) => {
   const { id: identityToken, uname } = request.auth;
   const { tenant, app } = request.params;
 
   // Get the App's URL:
-  const r1 = await request.pg.query(GET_APP, [app, uname, tenant]);
-  if (r1.rowCount === 0) {
+  const r1 = await request.pg.query(GET_SESSION_CLAIMS, [uname, tenant, app]);
+  if (r1.rowCount === 0 || r1.rows[0].app_info === null) {
     reply.status(404).send("App not found");
     return;
   }
-
-  // Get all the tenants
-  const tenants = await request.pg.query(
-    `
-    SELECT "tenant" FROM "public"."tenants"
-    WHERE "user" = $1
-  `,
-    [uname]
-  );
-
-  // Get all the apps
-  const apps = await request.pg.query(
-    `
-    SELECT "name" FROM "public"."catalog"
-    WHERE "user" = $1
-  `,
-    [uname]
-  );
 
   // Build the Refresh Token:
   // NOTE: this first Refresh Token is intended for immediate utilization
   //       from the target App as it will be passed down via URI parameter
   //       and it is particularly sensible to leakage.
   //       It takes the name of DELEGATION TOKEN
-  const r2 = await request.pg.query(GET_REFRESH_TOKEN, [
-    identityToken,
-    JSON.stringify({
-      uname,
-      tenant: tenants.rows.map(($) => $.tenant),
-      app: apps.rows.map(($) => $.name)
-    }),
-    "1m"
-  ]);
+  try {
+    const r2 = await request.pg.query(GET_REFRESH_TOKEN, [
+      identityToken,
+      JSON.stringify({
+        uname,
+        tenant: r1.rows[0].tenants,
+        app: r1.rows[0].apps
+      }),
+      "1m"
+    ]);
 
-  // Get data for the template:
-  const { url } = r1.rows[0];
-  const token = r2.rows[0].delegateToken;
+    // Get data for the template:
+    const { url } = r1.rows[0];
+    const token = r2.rows[0].delegate_token;
 
-  reply.type("text/html").send(`
+    reply.type("text/html").send(`
     <script>window.location = "${url}?token=${token}"</script>
   `);
+  } catch (err) {
+    console.log(err.message);
+    console.log(err);
+    throw err;
+  }
 };
